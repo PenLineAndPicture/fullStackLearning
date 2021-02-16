111991a35a6c648c849f79006d5b92d2ffb87bda
PenLineAndPicture <76979713+PenLineAndPicture@users.noreply.github.com>
2021-02-16 20:52:54 +0900

    2021-02-16
    20:52

--- 과제/1stPF/js/main.js
+++ 과제/1stPF/js/main.js
@@ -1,17 +1,6 @@
 //과제 - main.js
 
-//상품
-
-var merchandise = {
-    
-}
-
-
-
-
-
-
-
+var Img = document.getElementById("mainPhoto");
 
 /////////////////////// 로딩구역 ////////////////////
 window.addEventListener("DOMContentLoaded",function(){
@@ -23,6 +12,35 @@ window.addEventListener("DOMContentLoaded",function(){
 /////////////////////// 로딩구역 ////////////////////
 
 
-
-
-//아이템 넣기
+function gallist(seq){
+    
+    var Img = document.getElementById("mainPhoto");
+    
+    
+    /// 문제 리스트
+    //1. 클릭되면 이미지 넣기(색은 되는데, 이미지는 안됨)
+    //2. class "phototext"에 이름, 가격 넣기
+    //3. phototext에 넣은 텍스트들 css적용
+    
+    
+    if(seq === 0){
+        console.log("첫번째입니다.")
+        Img.style.backgroundColor = "red";
+    };
+    if(seq === 1){
+        console.log("두번째입니다.")
+        Img.style.backgroundColor = "yellow";
+    };
+    if(seq === 2){
+        console.log("세번째입니다.")
+        Img.style.backgroundColor = "blue";
+    };
+    if(seq === 3){
+        console.log("네번째입니다.")
+        Img.style.backgroundColor = "lime";
+    };
+    if(seq === 4){
+        console.log("다섯번째입니다.")
+        Img.style.backgroundColor = "purple";
+    };
+}
