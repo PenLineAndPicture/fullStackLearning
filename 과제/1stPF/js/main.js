18d661f27276944b28f9e9648841397d7d47b9c4
PenLineAndPicture <76979713+PenLineAndPicture@users.noreply.github.com>
2021-02-18 17:56:43 +0900

    2021-02-18
    17:56 myPF1

--- 과제/1stPF/js/main.js
+++ 과제/1stPF/js/main.js
@@ -1,46 +1,73 @@
 //과제 - main.js
 
+
 var Img = document.getElementById("mainPhoto");
 
 /////////////////////// 로딩구역 ////////////////////
-window.addEventListener("DOMContentLoaded",function(){
-    
+window.addEventListener("DOMContentLoaded", function () {
+
     // 1. 로드구역 확인
     console.log("로딩완료!");
-    
+
 });
 /////////////////////// 로딩구역 ////////////////////
 
+let txt = [
+    {
+        "제목": "11보석이야",
+        "내용": "11아주좋은거~~~~"
+},
+    {
+        "제목": "22멋진 보석이야",
+        "내용": "22Rmxrerere~~~~"
+}
 
-function gallist(seq){
+]; ////  txt ////////
+
+;
+
+
+function gallist(seq) {
+
+    //var Img = document.getElementById("mainPhoto");
+
+    let btns = $('.photo');
+    let btnsImg = btns.eq(seq).css("background-image").split("\"")[1].split("/");
+    btnsImg = btnsImg[btnsImg.length - 1];
+
+    console.log("이미지:" + btnsImg);
+
+    $("#mainPhoto").css({
+        background: "url(image/" + btnsImg + ") no-repeat center/cover"
+    }); ////////// css /////////////
     
-    var Img = document.getElementById("mainPhoto");
-    
-    
+    $(".ptit").text(txt[seq]["제목"]);
+    $(".pcont").text(txt[seq]["내용"]);
+
     /// 문제 리스트
     //1. 클릭되면 이미지 넣기(색은 되는데, 이미지는 안됨)
     //2. class "phototext"에 이름, 가격 넣기
     //3. phototext에 넣은 텍스트들 css적용
-    
-    
-    if(seq === 0){
-        console.log("첫번째입니다.")
-        Img.style.backgroundColor = "red";
-    };
-    if(seq === 1){
-        console.log("두번째입니다.")
-        Img.style.backgroundColor = "yellow";
-    };
-    if(seq === 2){
-        console.log("세번째입니다.")
-        Img.style.backgroundColor = "blue";
-    };
-    if(seq === 3){
-        console.log("네번째입니다.")
-        Img.style.backgroundColor = "lime";
-    };
-    if(seq === 4){
-        console.log("다섯번째입니다.")
-        Img.style.backgroundColor = "purple";
-    };
+
+    //    
+    //    if(seq === 0){
+    //        console.log("첫번째입니다.")
+    //        Img.style.backgroundColor = "red";
+    //    };
+    //    if(seq === 1){
+    //        console.log("두번째입니다.")
+    //        Img.style.backgroundColor = "yellow";
+    //    };
+    //    if(seq === 2){
+    //        console.log("세번째입니다.")
+    //        Img.style.backgroundColor = "blue";
+    //    };
+    //    if(seq === 3){
+    //        console.log("네번째입니다.")
+    //        Img.style.backgroundColor = "lime";
+    //    };
+    //    if(seq === 4){
+    //        console.log("다섯번째입니다.")
+    //        Img.style.backgroundColor = "purple";
+    //    };
 }
