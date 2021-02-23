d38a90f0765e1907c68e828e669ef16f3a304f96
PenLineAndPicture <76979713+PenLineAndPicture@users.noreply.github.com>
2021-02-23 17:57:14 +0900

    2021-02-23
    17:57

--- 과제/1stPF/js/main.js
+++ 과제/1stPF/js/main.js
@@ -1,24 +1,8 @@
 //과제 - main.js
-
-var i;
  
-var Img = document.getElementById("mainPhoto");
-
-/*
-photoSelect[i] = new Image();
 
 var photoSelect = ["image/ring.jpg", "image/earing.jpg", "image/necklace.jpg", "image/bracelet.jpg", "image/broche.jpg"]
-*/
 
-
-/////////////////////// 로딩구역 ////////////////////
-window.addEventListener("DOMContentLoaded", function () {
-
-    // 1. 로드구역 확인
-    console.log("로딩완료!");
-
-});
-/////////////////////// 로딩구역 ////////////////////
 
 let txt = [
     {
@@ -44,30 +28,65 @@ let txt = [
     {
         "제목": "Diamond leaf broche",
         "가격": "$ 220",
-        "내용": "Made of silver and tiny diamonds"
+        "내용": "Made of silver and tiny diamonds."
 }
 
 ]; ////  txt ////////
 
 
+/////////////////////// 로딩구역 ////////////////////
+window.addEventListener("DOMContentLoaded", function () {
+
+    
+//var Img = document.getElementById("mainPhoto");
+//
+//photoSelect[i] = new Image();
+//    
+//    
+//    // 1. 로드구역 확인
+//    console.log("로딩완료!");
+//    
+//    
+//
+//
+//document.getElementsByClassName("phototext")[0].style.fontFamily='Papyrus';
+
+    $("#mainPhoto").css({
+        background: "url(" + photoSelect[0] + ") no-repeat center/cover"
+    }); ////////// css /////////////
+    
+    $(".ptitle").text(txt[0]["제목"]);
+    $(".pprice").text(txt[0]["가격"]);
+    $(".pcont").text(txt[0]["내용"]);
+    
+    
+
+});
+/////////////////////// 로딩구역 ////////////////////
+
+
+
+
 
 function gallist(seq) {
 
+    console.log(seq+1 + "번입니다");
     //var Img = document.getElementById("mainPhoto");
 
     let btns = $('.photo');
-    let btnsImg = btns.eq(seq).css("background-image").split("\"")[1].split("/");
-    btnsImg = btnsImg[btnsImg.length - 1];
+//    let btnsImg = btns.eq(seq).css("background-image").split("\"")[1].split("/");
+//    btnsImg = btnsImg[btnsImg.length - 1];
 
-    console.log("이미지:" + btnsImg);
+//    console.log("이미지:" + btnsImg);
 
     $("#mainPhoto").css({
-        background: "url(image/" + btnsImg + ") no-repeat center/cover"
+        background: "url(" + photoSelect[seq] + ") no-repeat center/cover"
     }); ////////// css /////////////
     
-    $(".ptit").text(txt[seq]["제목"]);
+    $(".ptitle").text(txt[seq]["제목"]);
+    $(".pprice").text(txt[seq]["가격"]);
     $(".pcont").text(txt[seq]["내용"]);
-
+    
     /// 문제 리스트
     //1. 클릭되면 이미지 넣기(색은 되는데, 이미지는 안됨)
     //2. class "phototext"에 이름, 가격 넣기
@@ -96,7 +115,3 @@ function gallist(seq) {
     //        Img.style.backgroundColor = "purple";
     //    };
 }
-
-function gallist(seq){
-    console.log(seq+1 + "번입니다");
-};
